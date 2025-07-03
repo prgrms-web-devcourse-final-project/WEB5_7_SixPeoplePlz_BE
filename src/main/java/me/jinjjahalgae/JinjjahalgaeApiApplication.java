a4dd8e606bb2776f2b176c1360c024bb2f2589f7
Soohyeok Kim <devshk447@gmail.com>
2025-07-04 02:14:11 +0900

    init

---
+++ src/main/java/me/jinjjahalgae/JinjjahalgaeApiApplication.java
@@ -0,0 +1,13 @@
+package me.jinjjahalgae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinjjahalgaeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JinjjahalgaeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/jinjjahalgae/JinjjahalgaeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jinjjahalgae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinjjahalgaeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
