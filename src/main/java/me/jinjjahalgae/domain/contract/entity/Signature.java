7903cfad1342f9895bd434d9262b99b08df6c9b6
silkair <silkair1234@gmail.com>
2025-07-05 20:45:12 +0900

    Signature entity 생성

---
+++ src/main/java/me/jinjjahalgae/domain/contract/entity/Signature.java
@@ -0,0 +1,33 @@
+package me.jinjjahalgae.domain.contract.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Signature {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //서명 id
+
+    private Long contractId; //계약 id
+
+    private Long userId; //유저 id
+
+    private String imageKey; //이미지 key (url)
+
+    @Builder
+    public Signature(Long contractId, Long userId, String imageKey) {
+        this.contractId = contractId;
+        this.userId = userId;
+        this.imageKey = imageKey;
+    }
+}
