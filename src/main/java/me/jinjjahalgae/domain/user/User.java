b117dcef7ec913efd4d5d3e963bc1ee54c7f49cd
Soohyeok Kim <devshk447@gmail.com>
2025-07-06 21:43:09 +0900

    feat(user): 유저 도메인, 리포지터리 구현

--- src/main/java/me/jinjjahalgae/domain/user/User.java
+++ src/main/java/me/jinjjahalgae/domain/user/User.java
@@ -1,4 +1,33 @@
 package me.jinjjahalgae.domain.user;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.jinjjahalgae.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // 유저 id
+
+    private String name; // 이름
+
+    private String nickname; // 닉네임
+
+    private String email; // 이메일
+
+    @Builder
+    public User(Long id, String name, String nickname, String email) {
+        this.id = id;
+        this.name = name;
+        this.nickname = nickname;
+        this.email = email;
+    }
 }
