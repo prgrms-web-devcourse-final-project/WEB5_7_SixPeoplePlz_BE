2921d1095f16307a959a2d1460d2d96a571b75fe
Soohyeok Kim <devshk447@gmail.com>
2025-07-06 21:43:24 +0900

    feat(auth): auth 엔티티, 리포지터리 구현

---
+++ src/main/java/me/jinjjahalgae/domain/auth/Auth.java
@@ -0,0 +1,36 @@
+package me.jinjjahalgae.domain.auth;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.jinjjahalgae.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Auth extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // auth id
+
+    private String provider; // 써드파티 제공자
+
+    private String oauthId; // 써드파티 식별자
+
+    private String refreshToken; // 리프레시 토큰
+
+    private Long userId; // 유저 id (FK)
+
+    @Builder
+    public Auth(Long id, String provider, String oauthId, String refreshToken, Long userId) {
+        this.id = id;
+        this.provider = provider;
+        this.oauthId = oauthId;
+        this.refreshToken = refreshToken;
+        this.userId = userId;
+    }
+}
---
+++ src/main/java/me/jinjjahalgae/domain/auth/AuthRepository.java
@@ -0,0 +1,6 @@
+package me.jinjjahalgae.domain.auth;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthRepository extends JpaRepository<Auth, Long> {
+}
