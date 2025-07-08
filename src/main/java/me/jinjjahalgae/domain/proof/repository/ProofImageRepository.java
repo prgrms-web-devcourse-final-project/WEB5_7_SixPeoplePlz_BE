5c5c00a45d902778136cd101ad1077e67e45caf2
kwang2134 <kwang2134@naver.com>
2025-07-08 15:18:52 +0900

    feat(proof_create): @Repository 어노테이션 추가

--- src/main/java/me/jinjjahalgae/domain/proof/repository/ProofImageRepository.java
+++ src/main/java/me/jinjjahalgae/domain/proof/repository/ProofImageRepository.java
@@ -2,6 +2,8 @@ package me.jinjjahalgae.domain.proof.repository;
 
 import me.jinjjahalgae.domain.proof.entities.ProofImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProofImageRepository extends JpaRepository<ProofImage, Long> {
 }
--- src/main/java/me/jinjjahalgae/domain/proof/repository/ProofRepository.java
+++ src/main/java/me/jinjjahalgae/domain/proof/repository/ProofRepository.java
@@ -2,6 +2,8 @@ package me.jinjjahalgae.domain.proof.repository;
 
 import me.jinjjahalgae.domain.proof.entities.Proof;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProofRepository extends JpaRepository<Proof, Long> {
 }
