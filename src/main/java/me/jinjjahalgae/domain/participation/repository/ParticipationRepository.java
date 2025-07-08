74e562bca2f790a84fb3eef3cff0305abe8cc0ea
silkair <silkair1234@gmail.com>
2025-07-08 14:23:33 +0900

    fix(contract_init) : 파일 이름 수정

--- src/main/java/me/jinjjahalgae/domain/participation/repository/ParticipationRepository.java
+++ src/main/java/me/jinjjahalgae/domain/participation/repository/ParticipationRepository.java
@@ -3,5 +3,5 @@ package me.jinjjahalgae.domain.participation.repository;
 import me.jinjjahalgae.domain.participation.entity.Participation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SignatureRepository extends JpaRepository<Participation, Long> {
+public interface ParticipationRepository extends JpaRepository<Participation, Long> {
 }
