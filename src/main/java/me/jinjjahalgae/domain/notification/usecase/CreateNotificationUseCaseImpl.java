8a1c60ce3ff1a68afbffd6587060062a94034450
Seoyoon Tak <luckyoon0314@kaist.ac.kr>
2025-07-08 12:39:12 +0900

    feat(notification_init): 알림 생성 usecaseImpl 정의

---
+++ src/main/java/me/jinjjahalgae/domain/notification/usecase/CreateNotificationUseCaseImpl.java
@@ -0,0 +1,14 @@
+package me.jinjjahalgae.domain.notification.usecase;
+
+import me.jinjjahalgae.domain.notification.dto.NotificationCreateRequest;
+import me.jinjjahalgae.domain.notification.dto.NotificationCreateResponse;
+import me.jinjjahalgae.domain.notification.usecase.interfaces.CreateNotificationUseCase;
+
+
+public class CreateNotificationUseCaseImpl implements CreateNotificationUseCase {
+
+    @Override
+    public NotificationCreateResponse execute(NotificationCreateRequest request) {
+        return null;
+    }
+}
