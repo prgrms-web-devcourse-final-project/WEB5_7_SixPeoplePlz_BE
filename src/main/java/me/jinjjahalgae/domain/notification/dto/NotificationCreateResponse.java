1c7becb4682d39bb592654701a5fb1fcd183d866
Seoyoon Tak <luckyoon0314@kaist.ac.kr>
2025-07-08 15:43:08 +0900

    feat(notification_init): 알림 생성 응답 필드 수정

--- src/main/java/me/jinjjahalgae/domain/notification/dto/NotificationCreateResponse.java
+++ src/main/java/me/jinjjahalgae/domain/notification/dto/NotificationCreateResponse.java
@@ -2,11 +2,11 @@ package me.jinjjahalgae.domain.notification.dto;
 
 /**
  * 알림 생성 후 응답 dto
- * @param notificationId DB에 추가된 알림의 id
+ * @param notificationCnt DB에 추가된 알림의 개수
  * @param message 생성된 메세지 확인용
  */
 public record NotificationCreateResponse(
-        Long notificationId,
+        Long notificationCnt,
         String message
 ) {
 }
