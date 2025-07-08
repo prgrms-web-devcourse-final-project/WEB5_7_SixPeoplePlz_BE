114d233ca5f112ec44b90970094dcceb291dab3a
silkair <silkair1234@gmail.com>
2025-07-08 23:49:46 +0900

    fe

--- src/main/java/me/jinjjahalgae/domain/contract/usecase/ContractHistoryUseCase.java
+++
@@ -1,6 +0,0 @@
-package me.jinjjahalgae.domain.contract.usecase;
-
-public interface ContractHistoryUseCase {
-    //종료된 계약 조회
-    List<ContractListResponse> execute(Long userId, String type);
-}
--- src/main/java/me/jinjjahalgae/domain/contract/usecase/ContractListUseCase.java
+++ src/main/java/me/jinjjahalgae/domain/contract/usecase/ContractListUseCase.java
@@ -1,6 +1,7 @@
 package me.jinjjahalgae.domain.contract.usecase;
 
 public interface ContractListUseCase {
-    //계약 목록 조회
-    List<ContractListResponse> execute(Long userId);
+    //계약 목록 조회 (진행중, 종료 히스토리 공유)
+    List<ContractListResponse> execute(Long userId, String type);
+
 }
