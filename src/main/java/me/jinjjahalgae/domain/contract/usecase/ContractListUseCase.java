0d889d4cb64a89b8742069c089ed0b5d0d50efcc
silkair <silkair1234@gmail.com>
2025-07-09 00:06:04 +0900

    feat(contract) : 계약 목록 조회 파라미터 수정

--- src/main/java/me/jinjjahalgae/domain/contract/usecase/ContractListUseCase.java
+++ src/main/java/me/jinjjahalgae/domain/contract/usecase/ContractListUseCase.java
@@ -1,6 +1,10 @@
 package me.jinjjahalgae.domain.contract.usecase;
 
+import me.jinjjahalgae.domain.contract.enums.ContractStatus;
+
+import java.util.List;
+
 public interface ContractListUseCase {
     //계약 목록 조회 (진행중, 종료 히스토리 공유)
-    List<ContractListResponse> execute(Long userId, String type);
+    List<ContractListResponse> execute(Long userId, List<ContractStatus> statuses);
 }
