58d1212468041b05633065a097fbf96a8c8c1fbc
silkair <silkair1234@gmail.com>
2025-07-08 23:50:56 +0900

    feat(contract) : 계약 히스토리 조회 계약 목록 조회로 통합

--- src/main/java/me/jinjjahalgae/domain/contract/usecase/ContractListUseCase.java
+++ src/main/java/me/jinjjahalgae/domain/contract/usecase/ContractListUseCase.java
@@ -3,5 +3,4 @@ package me.jinjjahalgae.domain.contract.usecase;
 public interface ContractListUseCase {
     //계약 목록 조회 (진행중, 종료 히스토리 공유)
     List<ContractListResponse> execute(Long userId, String type);
-
 }
