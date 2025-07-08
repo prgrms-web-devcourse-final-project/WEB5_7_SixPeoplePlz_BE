163c5afee54721a893ef6ca8e0eb17b4d456a6a7
silkair <silkair1234@gmail.com>
2025-07-08 10:57:34 +0900

    feat(contract_init) : 엔티티 조ì서명 테이블 ENUM 추가 (역할, 유효성)

---
+++ src/main/java/me/jinjjahalgae/domain/signature/enums/Role.java
@@ -0,0 +1,6 @@
+package me.jinjjahalgae.domain.signature.enums;
+
+public enum Role {
+    CONTRACTOR,
+    SUPERVISOR
+}
---
+++ src/main/java/me/jinjjahalgae/domain/signature/enums/Validate.java
@@ -0,0 +1,6 @@
+package me.jinjjahalgae.domain.signature.enums;
+
+public enum Validate {
+    VALID,
+    INVALID
+}
