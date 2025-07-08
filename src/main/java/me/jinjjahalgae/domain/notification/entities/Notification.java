5a87818db57d1c9d4cc3d5803bc460bb3657f264
Seoyoon Tak <luckyoon0314@kaist.ac.kr>
2025-07-08 10:28:19 +0900

    feat(notification_init): 엔티티 id, 관계매핑 설명 추가

--- src/main/java/me/jinjjahalgae/domain/notification/entities/Notification.java
+++ src/main/java/me/jinjjahalgae/domain/notification/entities/Notification.java
@@ -6,6 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import me.jinjjahalgae.domain.notification.enums.NotificationType;
 
+/**
+ * - 알림을 클릭했을 때 계약 상세에 접근하게 될 예정임.
+ * 근데 해당 페이지에 접근할 때 알림을 받은 유저의 정보, 권한에 따라 접근을 막아야할 때도 있음.
+ * 알림 생성 시점과 알림 클릭 시점의 차이가 있을 수 있기 때문에 미리 유저와 계약의 추가적인 정보를 넘겨주면
+ * 의미가 없기 때문에 ID만 넘겨주는 것으로 결정함.
+ *
+ * - 프론트 측에서 페이지 이동을 위해 계약의 다른 필드에 대한 정보 없이 id만 있어도 될 것으로 판단함
+ */
 @Entity
 @Getter
 @NoArgsConstructor
