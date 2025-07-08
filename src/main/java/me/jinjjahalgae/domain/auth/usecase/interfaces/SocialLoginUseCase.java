bb927e3f697ade8f6bc1588316aa88e9df0e7431
Soohyeok Kim <devshk447@gmail.com>
2025-07-09 00:04:49 +0900

    fix(common): UseCase 인터페이스 제거

--- src/main/java/me/jinjjahalgae/domain/auth/usecase/interfaces/SocialLoginUseCase.java
+++ src/main/java/me/jinjjahalgae/domain/auth/usecase/interfaces/SocialLoginUseCase.java
@@ -2,8 +2,7 @@ package me.jinjjahalgae.domain.auth.usecase.interfaces;
 
 import me.jinjjahalgae.domain.auth.dto.login.SocialLoginRequest;
 import me.jinjjahalgae.domain.auth.dto.login.SocialLoginResponse;
-import me.jinjjahalgae.domain.common.UseCase;
 
-public interface SocialLoginUseCase extends UseCase<SocialLoginRequest, SocialLoginResponse> {
+public interface SocialLoginUseCase{
     SocialLoginResponse execute(SocialLoginRequest req);
 }
