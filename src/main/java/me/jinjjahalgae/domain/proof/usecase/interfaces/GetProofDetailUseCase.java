2bf3195408be2cd75348417dd4b5e38186a8e503
kwang2134 <kwang2134@naver.com>
2025-07-08 17:52:35 +0900

    feat(proof): 인증 상세 조회 유스케이스 정의

---
+++ src/main/java/me/jinjjahalgae/domain/proof/usecase/interfaces/GetProofDetailUseCase.java
@@ -0,0 +1,8 @@
+package me.jinjjahalgae.domain.proof.usecase.interfaces;
+
+import me.jinjjahalgae.domain.proof.dto.response.ProofDetailResponse;
+
+public interface GetProofDetailUseCase {
+
+    ProofDetailResponse execute(Long proofId);
+}
