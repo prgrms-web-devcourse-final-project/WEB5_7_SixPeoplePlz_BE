937b9053a7d737e4b85061240c337f29d81905f2
Seoyoon Tak <luckyoon0314@kaist.ac.kr>
2025-07-08 09:35:08 +0900

    feat(notification_init): 알림타입 enum 명칭 변경

--- src/main/java/me/jinjjahalgae/domain/notification/enums/NotificationType.java
+++ src/main/java/me/jinjjahalgae/domain/notification/enums/NotificationType.java
@@ -7,12 +7,11 @@ public enum NotificationType {
     CONTRACT_STARTED, // 계약 시작
     CONTRACT_ENDED, // 계약 종료
 
-    FEEDBACK_ADDED, // 피드백 등록
-
     PROOF_ADDED, // 인증 등록
     PROOF_ACCEPTED, // 인증 승인
     PROOF_REJECTED, // 인증 거절
 
-    // 재인증 등록... API에서는 again으로 썼던데 용어 이걸로 해도 괜찮을지..
-    RECERTIFICATION_ADDED
+    FEEDBACK_ADDED, // 피드백 등록
+
+    REPROOF_ADDED // 재인증 등록
 }
