5710968bd0544da0441d188910c361b904dab125
inswal843 <inswal843@gmail.com>
2025-07-09 14:52:49 +0900

    fix(invite): ContractRepository의 findByUuid 매개변수 소문자로 수정

--- src/main/java/me/jinjjahalgae/domain/contract/repository/ContractRepository.java
+++ src/main/java/me/jinjjahalgae/domain/contract/repository/ContractRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ContractRepository extends JpaRepository<Contract, Long> {
-    Optional<Contract> findByUuid(String Uuid);
+    Optional<Contract> findByUuid(String uuid);
 }
