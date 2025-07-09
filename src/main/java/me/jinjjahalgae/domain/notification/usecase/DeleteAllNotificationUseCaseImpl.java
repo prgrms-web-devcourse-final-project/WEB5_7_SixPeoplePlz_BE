9a5120f3c863443cc9da48b5cb915e595e47595e
Seoyoon Tak <luckyoon0314@kaist.ac.kr>
2025-07-09 17:06:38 +0900

    feat(notification_init): userid의 모든 알림 삭제 usecase 추가

---
+++ src/main/java/me/jinjjahalgae/domain/notification/usecase/DeleteAllNotificationUseCaseImpl.java
@@ -0,0 +1,28 @@
+package me.jinjjahalgae.domain.notification.usecase;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.jinjjahalgae.domain.notification.repository.NotificationRepository;
+import me.jinjjahalgae.domain.notification.usecase.interfaces.DeleteAllNotificationUseCase;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DeleteAllNotificationUseCaseImpl implements DeleteAllNotificationUseCase {
+
+    private final NotificationRepository notificationRepository;
+
+    /**
+     * userid에 해당하는 알림들을 일괄 삭제함
+     * @param userId 알림 일괄 삭제할 userid
+     * @return 삭제된 알림 개수
+     */
+    @Override
+    @Transactional // 트랜잭션 생성
+    public Long execute(Long userId) {
+
+        return notificationRepository.deleteAllByUserId(userId);
+    }
+}
