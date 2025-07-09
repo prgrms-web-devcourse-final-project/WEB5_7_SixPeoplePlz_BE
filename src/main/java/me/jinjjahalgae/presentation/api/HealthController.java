92ebdf30edba23c3400c32b9d8fc35aba4933055
Soohyeok Kim <devshk447@gmail.com>
2025-07-09 09:45:50 +0900

    fix(health): health 컨트롤러 swagger 명세

--- src/main/java/me/jinjjahalgae/presentation/api/HealthController.java
+++ src/main/java/me/jinjjahalgae/presentation/api/HealthController.java
@@ -1,8 +1,10 @@
 package me.jinjjahalgae.presentation.api;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Health API", description = "Health API")
 @RestController
 public class HealthController {
     @GetMapping("/health")
