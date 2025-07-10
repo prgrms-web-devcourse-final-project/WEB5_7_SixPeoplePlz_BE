2d905b412e833a8db400d6ed76c17177a7b6ff51
silkair <silkair1234@gmail.com>
2025-07-11 01:55:29 +0900

    feat(contract) : 계약자 권한 검증 updateContractUseCase 에 적용

--- src/main/java/me/jinjjahalgae/domain/contract/usecase/update/UpdateContractUseCaseImpl.java
+++ src/main/java/me/jinjjahalgae/domain/contract/usecase/update/UpdateContractUseCaseImpl.java
@@ -18,8 +18,13 @@ public class UpdateContractUseCaseImpl implements UpdateContractUseCase {
 
     @Override
     public void execute(Long userId, Long contractId, ContractUpdateRequest request) {
-        //수정 권한이 있는 계약자인가?
-        Contract contract = findContractByIdAndVerifyContractor(contractId, userId);
+        // 계약 조회
+        Contract contract = contractRepository.findById(contractId)
+                .orElseThrow(() -> ErrorCode.CONTRACT_NOT_FOUND.domainException("계약을 찾을 수 없습니다."));
+
+        // 계약자 권한 검증
+        contract.validateContractor(userId);
+
         //감독자 서명이 있는가? (시작 전에만 수정이 가능)
         contract.validateUpdatable();
         //계약 수정 진행
@@ -35,16 +40,5 @@ public class UpdateContractUseCaseImpl implements UpdateContractUseCase {
                 request.endDate(),
                 ContractType.valueOf(request.type())
         );
-    }
-
-    private Contract findContractByIdAndVerifyContractor(Long contractId, Long userId) {
-        //id로 계약을 찾고 없으면 없다고
-        Contract contract = contractRepository.findById(contractId)
-                .orElseThrow(() -> ErrorCode.CONTRACT_NOT_FOUND.domainException("계약을 찾을 수 없습니다."));
-        //있으면 계약자인지 확인(권한)
-        if (!contract.getUser().getId().equals(userId)) {
-            throw ErrorCode.ACCESS_DENIED.domainException("계약에 대한 접근 권한이 없습니다.");
-        }
-        return contract;
     }
 }
