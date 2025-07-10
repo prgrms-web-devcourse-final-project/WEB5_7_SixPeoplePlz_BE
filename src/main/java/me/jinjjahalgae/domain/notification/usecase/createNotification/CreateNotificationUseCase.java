4e50da16e8a2174d151695a2027437608cfa0bae
Seoyoon Tak <luckyoon0314@kaist.ac.kr>
2025-07-10 14:14:33 +0900

    docs(notification_init): 알림 생성 usecase 주석에 바뀐 명세 반영

--- src/main/java/me/jinjjahalgae/domain/notification/usecase/createNotification/CreateNotificationUseCase.java
+++ src/main/java/me/jinjjahalgae/domain/notification/usecase/createNotification/CreateNotificationUseCase.java
@@ -3,7 +3,7 @@ package me.jinjjahalgae.domain.notification.usecase.createNotification;
 /**
  * 알림 생성 usecase (백엔드에서만 쓰일 예정)
  * REQ : NotificationCreateRequest
- * RES : 알림id와 생성된 메세지를 담은 NotificationCreateResponse
+ * RES : 반환값 없음
  */
 public interface CreateNotificationUseCase {
 
