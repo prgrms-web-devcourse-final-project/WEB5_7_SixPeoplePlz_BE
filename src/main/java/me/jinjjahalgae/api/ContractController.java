ad3547409aa2a30ffa8cb4a3d6121c4e62429dbb
silkair <silkair1234@gmail.com>
2025-07-10 09:11:55 +0900

    feat(contract) : 계약 조회, 상세 조회 controller 구현

--- src/main/java/me/jinjjahalgae/api/ContractController.java
+++ src/main/java/me/jinjjahalgae/api/ContractController.java
@@ -4,14 +4,22 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import me.jinjjahalgae.domain.contract.dto.request.ContractCreateRequest;
 import me.jinjjahalgae.domain.contract.dto.response.ContractCreateResponse;
+import me.jinjjahalgae.domain.contract.dto.response.ContractDetailResponse;
+import me.jinjjahalgae.domain.contract.dto.response.ContractListResponse;
+import me.jinjjahalgae.domain.contract.enums.ContractStatus;
 import me.jinjjahalgae.domain.contract.usecase.ContractCreateUseCase;
+import me.jinjjahalgae.domain.contract.usecase.ContractDetailUseCase;
+import me.jinjjahalgae.domain.contract.usecase.ContractListUseCase;
 import me.jinjjahalgae.global.common.CommonResponse;
 import me.jinjjahalgae.global.security.jwt.CustomJwtPrincipal;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/contracts")
@@ -19,13 +27,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class ContractController {
 
     private final ContractCreateUseCase contractCreateUseCase;
+    private final ContractListUseCase contractListUseCase;
+    private final ContractDetailUseCase contractDetailUseCase;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public CommonResponse<ContractCreateResponse> createContract(
             @AuthenticationPrincipal CustomJwtPrincipal user,
             @Valid @RequestBody ContractCreateRequest request
     ) {
         ContractCreateResponse response = contractCreateUseCase.execute(user.getUserId(), request);
+        return CommonResponse.success(response);
+    }
+
+    @GetMapping
+    public CommonResponse<Page<ContractListResponse>> getContracts(
+            @AuthenticationPrincipal CustomJwtPrincipal user,
+            @RequestParam List<ContractStatus> statuses,
+            @PageableDefault(size = 10) Pageable pageable
+    ){
+        Page<ContractListResponse> response = contractListUseCase.execute(user.getUserId(), statuses, pageable);
+        return CommonResponse.success(response);
+    }
+
+    @GetMapping("/{contractId}")
+    public CommonResponse<ContractDetailResponse> getContractDetail(
+            @AuthenticationPrincipal CustomJwtPrincipal user,
+            @PathVariable Long contractId
+    ) {
+        ContractDetailResponse response = contractDetailUseCase.execute(user.getUserId(), contractId);
         return CommonResponse.success(response);
     }
 }
--- src/main/java/me/jinjjahalgae/domain/contract/repository/ContractRepository.java
+++ src/main/java/me/jinjjahalgae/domain/contract/repository/ContractRepository.java
@@ -6,6 +6,7 @@ import me.jinjjahalgae.domain.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +17,7 @@ public interface ContractRepository extends JpaRepository<Contract, Long> {
 
     //해당 유저의 계약 생태로 내림차순 조회
     Page<Contract> findByUserIdAndStatusInOrderByIdDesc(Long userId, List<ContractStatus> status, Pageable pageable);
+
     //사용자별 계약 상세 조회
-    Optional<Contract> findByIdAndUserIdWithParticipation(Long contractId, Long userId);
+    Optional<Contract> findByIdAndUserIdWithParticipation(@Param("contractId") Long contractId, @Param("userId") Long userId);
 }
