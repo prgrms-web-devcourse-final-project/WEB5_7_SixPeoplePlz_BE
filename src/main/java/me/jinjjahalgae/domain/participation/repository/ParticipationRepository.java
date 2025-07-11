1570fac86ac82b61b354c4ca91593cb1ca4f8410
Soohyeok Kim <devshk447@gmail.com>
2025-07-11 17:41:13 +0900

    fix(participation): 감독자 참가 usecase 유효성검사 추가

--- src/main/java/me/jinjjahalgae/domain/participation/repository/ParticipationRepository.java
+++ src/main/java/me/jinjjahalgae/domain/participation/repository/ParticipationRepository.java
@@ -1,15 +1,24 @@
 package me.jinjjahalgae.domain.participation.repository;
 
 import me.jinjjahalgae.domain.participation.entity.Participation;
+import me.jinjjahalgae.domain.participation.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
     // contract id가 일치하는 모든 participation 객체를 리스트로 조회
-    List<Participation> findByContract_Id(Long contractId);
+    List<Participation> findByContractId(Long contractId);
 
     // 해당 유저가 계약의 참여자인지
     boolean existsByContractIdAndUserId(Long contractId, Long userId);
+
+    // 해당 유저가 계약의 참여자고 해당 role을 가지고 있는지
+    boolean existsByContractIdAndUserIdAndRole(Long contractId, Long userId, Role role);
+
+    // 해당 유저가 계약의 참여중이고 role을 가지고 있고 유효한 상태인지
+    boolean existsByContractIdAndUserIdAndRoleAndValid(Long contractId, Long userId, Role role, boolean valid);
+
+    // 해당 유저가 계약의 참여자고 특정 role을 가지고 있는지
+    boolean existsByContractIdAndRole(Long contractId, Role role);
 }
