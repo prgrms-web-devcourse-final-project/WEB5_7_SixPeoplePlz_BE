7cce85bef4877e425edc086d44ef71cae7bb6271
kwang2134 <kwang2134@naver.com>
2025-07-14 10:15:21 +0900

    refactor(proof): 재인증 가져오는 방식 변경, 날짜 기반 -> 원본 인증 id 기반

--- src/main/java/me/jinjjahalgae/domain/proof/repository/ProofRepository.java
+++ src/main/java/me/jinjjahalgae/domain/proof/repository/ProofRepository.java
@@ -126,19 +126,16 @@ ORDER BY p.createdAt ASC
     /**
      * 계약자용 해당 달의 모든 재인증 id를 가져오는 쿼리
      * @param contractId 계약 id
-     * @param startDate 시작일 (월 기준 첫날 00:00:00)
-     * @param endDate 종료일 (월 기준 마지막 날 23:59:59:999999999)
+     * @param proofIds 원본 인증 id들
      * @return Long
      */
     @Query("""
 SELECT p.id
 FROM Proof p
 WHERE p.contractId = :contractId
-AND p.createdAt >= :startDate
-AND p.createdAt <= :endDate
-AND p.proofId IS NOT NULL
+AND p.proofId IN :proofIds
 """)
-    List<Long> findReProofIdsByMonth(@Param("contractId")Long contractId, @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
+    List<Long> findReProofIdsByMonth(@Param("contractId")Long contractId, List<Long> proofIds);
 
     /**
      * 감독자용 해당 달의 모든 원본 인증 id를 가져오는 쿼리
@@ -170,17 +167,14 @@ ORDER BY p.createdAt ASC
     /**
      * 감독자용 해당 달의 모든 재인증 id를 가져오는 쿼리
      * @param contractId 계약 id
-     * @param startDate 시작일 (월 기준 첫날 00:00:00)
-     * @param endDate 종료일 (월 기준 마지막 날 23:59:59:999999999)
+     * @param proofIds 원본 인증 id들
      * @return Long
      */
     @Query("""
 SELECT p.id
 FROM Proof p
 WHERE p.contractId = :contractId
-AND p.createdAt >= :startDate
-AND p.createdAt <= :endDate
-AND p.proofId IS NOT NULL
+AND p.proofId IN :proofIds
 AND EXISTS (
 SELECT 1
 FROM Feedback f
@@ -189,8 +183,7 @@ AND f.userId = :userId
 )
 """)
     List<Long> findReProofIdsByMonthForSupervisor(@Param("contractId")Long contractId,
-                                                  @Param("startDate") LocalDateTime startDate,
-                                                  @Param("endDate") LocalDateTime endDate,
+                                                  @Param("proofIds") List<Long> proofIds,
                                                   @Param("userId") Long userId);
 
     @Query("SELECT p FROM Proof p WHERE p.createdAt <= :deadline AND p.status = 'APPROVE_PENDING'")
