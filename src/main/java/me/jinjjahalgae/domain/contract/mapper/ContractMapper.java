40a03056621c25cd2108b36c66bd4aeec4198c45
Seoyoon Tak <luckyoon0314@kaist.ac.kr>
2025-07-14 09:42:32 +0900

    feat(contract-preview): 계약, 참여 mapper 추가

--- src/main/java/me/jinjjahalgae/domain/contract/mapper/ContractMapper.java
+++ src/main/java/me/jinjjahalgae/domain/contract/mapper/ContractMapper.java
@@ -1,5 +1,6 @@
 package me.jinjjahalgae.domain.contract.mapper;
 
+import lombok.RequiredArgsConstructor;
 import me.jinjjahalgae.domain.contract.usecase.create.dto.CreateContractRequest;
 import me.jinjjahalgae.domain.contract.usecase.get.common.ContractBasicResponse;
 import me.jinjjahalgae.domain.contract.usecase.get.detail.dto.ContractDetailResponse;
@@ -7,8 +8,8 @@ import me.jinjjahalgae.domain.contract.usecase.get.list.dto.ContractListResponse
 import me.jinjjahalgae.domain.contract.entity.Contract;
 import me.jinjjahalgae.domain.contract.enums.ContractType;
 import me.jinjjahalgae.domain.contract.usecase.get.preview.dto.ContractPreviewResponse;
-import me.jinjjahalgae.domain.contract.usecase.get.preview.dto.SignatureInfoResponse;
 import me.jinjjahalgae.domain.participation.entity.Participation;
+import me.jinjjahalgae.domain.participation.mapper.ParticipationMapper;
 import me.jinjjahalgae.domain.participation.usecase.common.ParticipantSimpleResponse;
 import me.jinjjahalgae.domain.user.User;
 import org.springframework.stereotype.Component;
@@ -16,7 +17,11 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class ContractMapper {
+
+    private final ParticipationMapper participationMapper;
+
     public Contract toEntity(User user, CreateContractRequest request) {
         Contract contract = Contract.builder()
                 .user(user)
@@ -55,8 +60,8 @@ public class ContractMapper {
         );
     }
 
-    public ContractDetailResponse toDetailResponse(Contract contract) {
-        ContractBasicResponse basicResponse = new ContractBasicResponse(
+    public ContractBasicResponse toBasicResponse(Contract contract) {
+        return new ContractBasicResponse(
                 contract.getId(),
                 contract.getUuid(),
                 contract.getTitle(),
@@ -69,7 +74,11 @@ public class ContractMapper {
                 contract.getStartDate(),
                 contract.getEndDate()
         );
+    }
 
+    public ContractDetailResponse toDetailResponse(Contract contract) {
+
+        ContractBasicResponse basicResponse = toBasicResponse(contract);
         return new ContractDetailResponse(
                 basicResponse,
                 contract.getStatus(),
@@ -95,12 +104,13 @@ public class ContractMapper {
                 .toList();
     }
 
-    public ContractPreviewResponse toPreviewResponse(Contract contract, List<Participation> participationList) {
+    public ContractPreviewResponse mapToContractPreviewResponse(Contract contract, List<Participation> participationList) {
         return new ContractPreviewResponse(
-                toDetailResponse(contract),
-                participationList.stream()
-                        .map(SignatureInfoResponse::from)
-                        .toList()
+            toBasicResponse(contract),
+            contract.getType(),
+            participationList.stream()
+                    .map(participationMapper::mapToFullResponse)
+                    .toList()
         );
     }
 }
--- src/main/java/me/jinjjahalgae/domain/contract/usecase/get/preview/dto/ContractPreviewResponse.java
+++ src/main/java/me/jinjjahalgae/domain/contract/usecase/get/preview/dto/ContractPreviewResponse.java
@@ -1,17 +1,20 @@
 package me.jinjjahalgae.domain.contract.usecase.get.preview.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import me.jinjjahalgae.domain.contract.usecase.get.detail.dto.ContractDetailResponse;
+import me.jinjjahalgae.domain.contract.enums.ContractType;
+import me.jinjjahalgae.domain.contract.usecase.get.common.ContractBasicResponse;
+import me.jinjjahalgae.domain.participation.usecase.common.ParticipantFullResponse;
 
 import java.util.List;
 
 public record ContractPreviewResponse(
 
-        @Schema(description = "계약 제목, 기간 등 계약서의 세부 정보")
-        ContractDetailResponse contractDetail,
+        @Schema(description = "계약 조회 공통 데이터")
+        ContractBasicResponse contractBasicResponse,
 
-        @Schema(description = "서명 정보 리스트 (유저이름, 역할, 이미지 키")
-        List<SignatureInfoResponse> signatureInfoList
-) {
+        @Schema(description = "계약서 디자인 타입", example = "BASIC")
+        ContractType type, //계약서 디자인
 
-}
+        @Schema(description = "참여자 정보", example = "참여자 정보")
+        List<ParticipantFullResponse> participants //참여한 유저 정보
+){}
