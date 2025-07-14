5c209c8e0ba151744533b2912e79f807f59e0277
Soohyeok Kim <devshk447@gmail.com>
2025-07-15 02:19:03 +0900

    fix(proof):  dto 시간 처리 utc로 받고 내리도록 수정

--- src/main/java/me/jinjjahalgae/domain/proof/usecase/getlist/contractorlist/dto/ContractorProofListResponse.java
+++ src/main/java/me/jinjjahalgae/domain/proof/usecase/getlist/contractorlist/dto/ContractorProofListResponse.java
@@ -3,7 +3,7 @@ package me.jinjjahalgae.domain.proof.usecase.getlist.contractorlist.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import me.jinjjahalgae.domain.proof.usecase.getlist.common.ProofSimpleResponse;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 /**
  * 계약자 달력 표시를 위한 response
  * 재인증 필드는 재인증이 존재하지 않는 경우 null
@@ -32,13 +32,13 @@ public record ContractorProofListResponse(
         String date,
 
         @Schema(description = "계약의 종료일 (종료 2일 전 재인증 요청 불가 검증에 사용할 필드)")
-        LocalDateTime endDate,
+        Instant endDate,
 
         @Schema(description = "원본 인증 데이터")
         ProofSimpleResponse originalProof,
 
         @Schema(description = "인증이 거절처리된 시간 (재인증 요청 버튼 출력용)")
-        LocalDateTime rejectedAt,
+        Instant rejectedAt,
 
         @Schema(description = "재인증 데이터 (해당일에 재인증이 존재하지 않으면 null)")
         ProofSimpleResponse reProof
