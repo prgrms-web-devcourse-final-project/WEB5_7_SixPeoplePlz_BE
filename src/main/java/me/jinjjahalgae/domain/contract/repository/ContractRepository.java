cc32605d213d86c2c95788b83872bcb00e9090e5
Seoyoon Tak <luckyoon0314@kaist.ac.kr>
2025-07-14 16:24:27 +0900

    feat(contract-preview): 리팩토링 todo 추가

--- src/main/java/me/jinjjahalgae/domain/contract/repository/ContractRepository.java
+++ src/main/java/me/jinjjahalgae/domain/contract/repository/ContractRepository.java
@@ -7,11 +7,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import me.jinjjahalgae.domain.participation.enums.Role;
@@ -45,6 +43,7 @@ public interface ContractRepository extends JpaRepository<Contract, Long> {
     @Query("SELECT c FROM Contract c JOIN FETCH c.user WHERE c.id = :contractId")
     Optional<Contract> findByIdWithUser(@Param("contractId") Long contractId);
 
+    // TODO: 나중에 mroonga로 리팩토링 고려해보기
     // 계약 히스토리 검색 (role, keyword, endDate, status 조건)
     @Query("""
         SELECT DISTINCT c 
