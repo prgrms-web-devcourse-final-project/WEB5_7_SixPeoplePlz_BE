21c7288d315ab7b35d3d2c7290289d8dbd6f6fba
kwang2134 <kwang2134@naver.com>
2025-07-15 12:02:40 +0900

    refactor(proof): 인증 생성 존재하지 않는 계약 검색 시 404 추가

--- src/main/java/me/jinjjahalgae/domain/proof/usecase/create/reproof/CreateReProofUseCaseImpl.java
+++ src/main/java/me/jinjjahalgae/domain/proof/usecase/create/reproof/CreateReProofUseCaseImpl.java
@@ -43,14 +43,11 @@ public class CreateReProofUseCaseImpl implements CreateReProofUseCase {
 
         // 계약 조회 - 계약이 존재하는 지 검증 겸용
         Long contractId = proof.getContractId();
-        Contract contract = contractRepository.findById(contractId)
+        Contract contract = contractRepository.findByIdWithUser(contractId)
                 .orElseThrow(() -> ErrorCode.CONTRACT_NOT_FOUND.domainException(contractId + "에 대한 계약이 존재하지 않습니다."));
 
-        // 유저의 계약인지 확인
-        boolean isUserContract = contractRepository.existsByIdAndUserId(contractId, userId);
-
         // 유저의 계약이 아닐 경우 예외
-        if (!isUserContract) {
+        if (!isUserContract(contract, userId)) {
             throw ErrorCode.ACCESS_DENIED.domainException("계약에 대한 접근 권한이 없습니다.");
         }
 
@@ -100,6 +97,12 @@ public class CreateReProofUseCaseImpl implements CreateReProofUseCase {
         ));
     }
 
+    // 유저의 계약인지 확인하는 메서드
+    private boolean isUserContract(Contract contract, Long userId) {
+        return contract.getUser().getId().equals(userId);
+    }
+
+    // 계약이 진행중인지 확인하는 메서드
     private boolean isInProgressContract(Contract contract) {
         return contract.getStatus() == ContractStatus.IN_PROGRESS;
     }
