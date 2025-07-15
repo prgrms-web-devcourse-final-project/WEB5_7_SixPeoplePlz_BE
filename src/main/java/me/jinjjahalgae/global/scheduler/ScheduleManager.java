20b1c34e3300154ec35dc8bdde16e4e475d87f0c
inswal843 <inswal843@gmail.com>
2025-07-15 16:18:17 +0900

    refactor(scheduler): 계약 시작, 종료, 주간 인증 점검 스케줄러를 글로벌 스케줄러로 변경

--- src/main/java/me/jinjjahalgae/global/scheduler/ScheduleManager.java
+++ src/main/java/me/jinjjahalgae/global/scheduler/ScheduleManager.java
@@ -1,8 +1,7 @@
 package me.jinjjahalgae.global.scheduler;
 
 import lombok.RequiredArgsConstructor;
-import me.jinjjahalgae.domain.contract.usecase.process.EndOneOffContractUseCase;
-import me.jinjjahalgae.domain.contract.usecase.process.VerifyOneOffContractSignatureUseCase;
+import me.jinjjahalgae.domain.contract.usecase.process.*;
 import me.jinjjahalgae.domain.proof.usecase.schedule.CheckExpiredProofUseCase;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -17,6 +16,10 @@ public class ScheduleManager {
     private final EndOneOffContractUseCase endOneOffContractUseCase;
     private final VerifyOneOffContractSignatureUseCase verifyOneOffContractSignatureUseCase;
     private final CheckExpiredProofUseCase checkExpiredProofUseCase;
+
+    private final StartContractsUseCase startContractsUseCase;
+    private final CheckContractsWeeklyProofUseCase checkContractsWeeklyProofUseCase;
+    private final EndContractsUseCase endContractsUseCase;
 
     /**
      * 5분마다 실행되는 스케줄러
@@ -36,5 +39,25 @@ public class ScheduleManager {
 
         // 인증 24시간(만료) 체크
         checkExpiredProofUseCase.execute(LocalDateTime.now());
+    }
+
+    /**
+     * 매일 00:01 에 실행되는 스케줄러
+     *
+     * - 단건 계약 24시간 체크
+     * - 단건 계약 서명검증
+     * - 인증 24시간(만료) 체크
+     */
+    @Scheduled(cron = "0 1 0 * * *", zone = "Asia/Seoul" )
+    @Transactional
+    public void daySchedule() {
+        // 시작일 확인 후 계약 시작
+        startContractsUseCase.execute();
+
+        // 7일 + 3일(재인증 유예시간)이 지나면 주간 인증 현황 체크
+        checkContractsWeeklyProofUseCase.execute();
+
+        // 성공 실패를 판단 후 계약 종료
+        endContractsUseCase.execute();
     }
 }
