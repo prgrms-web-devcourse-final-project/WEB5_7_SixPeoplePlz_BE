18f9135e91771338ac9632e68f5be8788db86f69
inswal843 <inswal843@gmail.com>
2025-07-16 12:57:20 +0900

    refactor(scheduler-contract): 계약 시작시 삭제된 계약의 알림은 usecaes로 바로 보내도록 수정

--- src/main/java/me/jinjjahalgae/domain/contract/usecase/process/StartContractsUseCaseImpl.java
+++ src/main/java/me/jinjjahalgae/domain/contract/usecase/process/StartContractsUseCaseImpl.java
@@ -5,18 +5,16 @@ import me.jinjjahalgae.domain.contract.entity.Contract;
 import me.jinjjahalgae.domain.contract.enums.ContractStatus;
 import me.jinjjahalgae.domain.contract.repository.ContractRepository;
 import me.jinjjahalgae.domain.notification.enums.NotificationType;
+import me.jinjjahalgae.domain.notification.usecase.create.CreateNotificationUseCase;
+import me.jinjjahalgae.domain.notification.usecase.create.dto.NotificationCreateRequest;
 import me.jinjjahalgae.domain.notification.usecase.listener.event.NotificationEvent;
-import me.jinjjahalgae.domain.proof.repository.ProofRepository;
 import me.jinjjahalgae.global.storage.redis.usecase.invite.bulk.BulkDeleteInviteInfoUseCase;
 import me.jinjjahalgae.global.storage.redis.usecase.invite.get.GetJoinedSupervisorsUseCase;
-import me.jinjjahalgae.global.util.UtcDateTimeUtil;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneOffset;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -29,6 +27,7 @@ public class StartContractsUseCaseImpl implements StartContractsUseCase {
     private final BulkDeleteInviteInfoUseCase bulkdeleteInviteInfoUseCase;
     private final GetJoinedSupervisorsUseCase getJoinedSupervisorsUseCase;
     private final ApplicationEventPublisher eventPublisher;
+    private final CreateNotificationUseCase createNotificationUseCase;
 
     @Override
     @Transactional
@@ -64,7 +63,7 @@ public class StartContractsUseCaseImpl implements StartContractsUseCase {
             contractRepository.deleteAll(deleteContracts);
 
             deleteContracts.forEach(contract ->
-                    eventPublisher.publishEvent(new NotificationEvent(
+                    createNotificationUseCase.execute(new NotificationCreateRequest(
                             NotificationType.CONTRACT_AUTO_DELETED,
                             contract.getId(),
                             contract.getUser().getId()
