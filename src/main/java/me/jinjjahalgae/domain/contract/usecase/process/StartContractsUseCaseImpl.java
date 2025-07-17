e7e7fd930034ce128eb16dbe6ebc7c7edfecb41b
inswal843 <inswal843@gmail.com>
2025-07-17 12:11:26 +0900

    refactor(scheduler-contract): 계약 시작, 종료시 알림 일괄 전송

--- src/main/java/me/jinjjahalgae/domain/contract/usecase/process/StartContractsUseCaseImpl.java
+++ src/main/java/me/jinjjahalgae/domain/contract/usecase/process/StartContractsUseCaseImpl.java
@@ -5,8 +5,10 @@ import me.jinjjahalgae.domain.contract.entity.Contract;
 import me.jinjjahalgae.domain.contract.enums.ContractStatus;
 import me.jinjjahalgae.domain.contract.repository.ContractRepository;
 import me.jinjjahalgae.domain.notification.enums.NotificationType;
+import me.jinjjahalgae.domain.notification.model.NotificationData;
 import me.jinjjahalgae.domain.notification.usecase.create.CreateNotificationUseCase;
 import me.jinjjahalgae.domain.notification.usecase.create.dto.NotificationCreateRequest;
+import me.jinjjahalgae.domain.notification.usecase.listener.event.NotificationBatchEvent;
 import me.jinjjahalgae.domain.notification.usecase.listener.event.NotificationEvent;
 import me.jinjjahalgae.global.storage.redis.usecase.invite.bulk.BulkDeleteInviteInfoUseCase;
 import me.jinjjahalgae.global.storage.redis.usecase.invite.get.GetJoinedSupervisorsUseCase;
@@ -48,23 +50,20 @@ public class StartContractsUseCaseImpl implements StartContractsUseCase {
         List<Contract> startContracts = pendingContractsBySupervisors.get(true);
         List<Contract> deleteContracts = pendingContractsBySupervisors.get(false);
 
-        // 계약 시작 처리
-        for (Contract contract : startContracts) {
-            int joinedSupervisors = getJoinedSupervisorsUseCase.execute(contract.getId());
-            contract.start(joinedSupervisors);
-
-            eventPublisher.publishEvent(new NotificationEvent(
-                            NotificationType.CONTRACT_STARTED,
-                            contract.getId(),
-                            contract.getUser().getId()
-                    )
-            );
+        // 계약 시작 처리 및 알림 배치
+        if (!startContracts.isEmpty()) {
+            for (Contract contract : startContracts) {
+                int joinedSupervisors = getJoinedSupervisorsUseCase.execute(contract.getId());
+                contract.start(joinedSupervisors);
+            }
+            createBatchNotifications(startContracts, NotificationType.CONTRACT_STARTED);
         }
 
         // 감독자 부족 계약 전체 삭제
         if (!deleteContracts.isEmpty()) {
             contractRepository.deleteAll(deleteContracts);
 
+            // 트랜잭션 분리로 usecase를 불러와 알림 전송
             deleteContracts.forEach(contract ->
                     createNotificationUseCase.execute(new NotificationCreateRequest(
                             NotificationType.CONTRACT_AUTO_DELETED,
@@ -77,5 +76,17 @@ public class StartContractsUseCaseImpl implements StartContractsUseCase {
         // 처리된 계약의 redis 정보 일괄 삭제
         List<Long> allProcessedIds = pendingContracts.stream().map(Contract::getId).toList();
         bulkdeleteInviteInfoUseCase.execute(allProcessedIds);
+    }
+
+    private void createBatchNotifications(List<Contract> contracts, NotificationType type) {
+        if (contracts == null || contracts.isEmpty()) {
+            return;
+        }
+
+        List<NotificationData> notificationDataList = contracts.stream()
+                .map(contract -> new NotificationData(contract.getId(), contract.getUser().getId()))
+                .toList();
+
+        eventPublisher.publishEvent(new NotificationBatchEvent(type, notificationDataList));
     }
 }
