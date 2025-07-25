cac698dcf6436fb3a4220ff6f702596101fd026c
inswal843 <inswal843@gmail.com>
2025-07-26 01:04:17 +0900

    feat: 시작점에 Retryable 어노테이션 추가

--- src/main/java/me/jinjjahalgae/JinjjahalgaeApiApplication.java
+++ src/main/java/me/jinjjahalgae/JinjjahalgaeApiApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
 @ConfigurationPropertiesScan
+@Retryable
 public class JinjjahalgaeApiApplication {
 
     public static void main(String[] args) {
